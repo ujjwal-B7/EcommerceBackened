ebf1a1585bea0a5b9ebe674725b44d8745b5a8cd
SLASH-7 <basnetujjwal98@gmail.com>
2023-11-22 23:49:27 +0545

    duplicate key error working

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -10,6 +10,12 @@ module.exports = (err, req, res, next) => {
     err = new ErrorHandler(`${err.message}`, 400);
   }
 
+  // mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered.`;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     error: err.message,
