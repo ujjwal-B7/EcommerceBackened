7dae58cbf3e015d78d1fc3d3194b0a8e1ec8c35d
SLASH-7 <basnetujjwal98@gmail.com>
2023-11-23 13:25:10 +0545

    duplicate error done

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -6,13 +6,14 @@ module.exports = (err, req, res, next) => {
   err.message = err.message || "Internal Server Error";
 
   // wrong mongodb id error
-  if ((err.name = "CASTERROR")) {
+  if (err.name === "CASTERROR") {
     err = new ErrorHandler(`${err.message}`, 400);
   }
 
   // mongoose duplicate key error
-  if (err.code === 11000) {
-    const message = `Duplicate ${Object.keys(err.keyValue)} Entered.`;
+  if (err.message && err.message.includes("duplicate key error")) {
+    const duplicateKeyField = err.message.match(/index: ([^_]+)_/)[1];
+    const message = `Duplicate ${duplicateKeyField} Email is already registered.`;
     err = new ErrorHandler(message, 400);
   }
 
