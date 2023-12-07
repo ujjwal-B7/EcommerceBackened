931839a0a6b8ce190b44d020f5029ee21b088478
SLASH-7 <basnetujjwal98@gmail.com>
2023-12-07 23:25:03 +0545

    order model created

---
+++ backend/model/orderModel.js
@@ -0,0 +1,92 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const orderSchema = new Schema({
+  shippingInfo: {
+    address: {
+      type: String,
+      required: [true, "Please enter the address"],
+    },
+    city: {
+      type: String,
+      required: [true, "Please enter the city"],
+    },
+    Province: {
+      type: String,
+      required: [true, "Please enter the province"],
+    },
+    phoneNumber: {
+      type: Number,
+      required: [true, "Please enter the phone number"],
+    },
+  },
+  orderItems: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      image: {
+        type: String,
+        required: true,
+      },
+      product: {
+        type: mongoose.Schema.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+    },
+  ],
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  paymentInfo: {
+    id: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+  },
+  paidAt: {
+    type: Date,
+    required: true,
+  },
+  itemsPrice: {
+    type: Number,
+    default: 0,
+    required: true,
+  },
+  shippingPrice: {
+    type: Number,
+    default: 0,
+    required: true,
+  },
+  totalPrice: {
+    type: Number,
+    default: 0,
+    required: true,
+  },
+  orderStatus: {
+    type: String,
+    required: true,
+    default: "Processing",
+  },
+  deliveredAt: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+module.exports = mongoose.model("Order", orderSchema);
