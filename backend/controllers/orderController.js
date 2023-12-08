7280366c0d475e274d40144563d9cd26d133386a
SLASH-7 <basnetujjwal98@gmail.com>
2023-12-08 22:29:03 +0545

    get single order by admin to review and also by users to see thier order history done

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -17,28 +17,28 @@ exports.createOrder = catchErrors(async (req, res, next) => {
 
   //   demo data
   //  {
-//   "itemsPrice":200,
-//   "shippingPrice":100,
-//   "totalPrice":300,
-//   "orderItems":[{
-//     "product":"6568ce932eeab2c88f80477d",
-//     "name":"guitar",
-//     "price":"1500",
-//     "image":"sample image",
-//     "quantity":1
-//   }
-// ],
-// "shippingInfo":{
-//   "address":"ktm",
-//   "city":"ktm",
-//   "Province":"Bagmati",
-//   "phoneNumber":9814313435
-// },
-// "paymentInfo":{
-//   "id":"sample paymentInfo",
-//   "status":"succceded"
-// }
-// }
+  //   "itemsPrice":200,
+  //   "shippingPrice":100,
+  //   "totalPrice":300,
+  //   "orderItems":[{
+  //     "product":"6568ce932eeab2c88f80477d",
+  //     "name":"guitar",
+  //     "price":"1500",
+  //     "image":"sample image",
+  //     "quantity":1
+  //   }
+  // ],
+  // "shippingInfo":{
+  //   "address":"ktm",
+  //   "city":"ktm",
+  //   "Province":"Bagmati",
+  //   "phoneNumber":9814313435
+  // },
+  // "paymentInfo":{
+  //   "id":"sample paymentInfo",
+  //   "status":"succceded"
+  // }
+  // }
 
   const order = await Order.create({
     shippingInfo,
@@ -54,5 +54,29 @@ exports.createOrder = catchErrors(async (req, res, next) => {
     success: true,
     message: "Order placed successfully.",
     order,
+  });
+});
+
+// get single order by admin
+exports.getSingleOrder = catchErrors(async (req, res, next) => {
+  const singleOrder = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (!singleOrder) {
+    return next(new ErrorHandler("No order found.", 404));
+  }
+  res.status(200).json({
+    success: true,
+    singleOrder,
+  });
+});
+
+// get my orders
+exports.getTotalOrdersByUser = catchErrors(async (req, res, next) => {
+  const myOrders = await Order.find({ user: req.user.id });
+  res.status(200).json({
+    success: true,
+    myOrders,
   });
 });
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -4,5 +4,16 @@ const orderController = require("../controllers/orderController");
 const { authenticatedUser, authorizedRoles } = require("../middleware/auth");
 
 router.post("/order/new", authenticatedUser, orderController.createOrder);
+router.get(
+  "/order/:id",
+  authenticatedUser,
+  authorizedRoles("admin"),
+  orderController.getSingleOrder
+);
+router.get(
+  "/orders/myOrders",
+  authenticatedUser,
+  orderController.getTotalOrdersByUser
+);
 
 module.exports = router;
