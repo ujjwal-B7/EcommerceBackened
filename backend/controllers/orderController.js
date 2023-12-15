f048e1e40df487c48bb19fa867de1950f0b23ccb
SLASH-7 <basnetujjwal98@gmail.com>
2023-12-15 21:14:46 +0545

    delete order done

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -57,7 +57,7 @@ exports.createOrder = catchErrors(async (req, res, next) => {
   });
 });
 
-// get single order by admin
+// get single order by admin to know how many users ordered the same product
 exports.getSingleOrder = catchErrors(async (req, res, next) => {
   const singleOrder = await Order.findById(req.params.id).populate(
     "user",
@@ -72,11 +72,68 @@ exports.getSingleOrder = catchErrors(async (req, res, next) => {
   });
 });
 
-// get my orders
+// get my orders by the user to see their order history
 exports.getTotalOrdersByUser = catchErrors(async (req, res, next) => {
   const myOrders = await Order.find({ user: req.user.id });
   res.status(200).json({
     success: true,
     myOrders,
+  });
+});
+
+// get all orders by Admin
+exports.getTotalOrdersByAdmin = catchErrors(async (req, res, next) => {
+  const orders = await Order.find();
+  let totalAmount = 0;
+  for (const item of orders) {
+    totalAmount += item.totalPrice;
+  }
+  res.status(200).json({
+    success: true,
+    totalAmount,
+    orders,
+  });
+});
+
+// update order status by admin
+exports.updateOrderStatus = catchErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+  if (order.orderStatus === "Delivered") {
+    return next(
+      new ErrorHandler("Your product has already been delivered", 404)
+    );
+  }
+
+  order.orderItems.forEach(async (order) => {
+    await updateStock(order.product, order.quantity);
+  });
+  order.orderStatus = req.body.orderStatus;
+  if (req.body.orderStatus === "Delivered") {
+    order.deliveredAt = Date.now();
+  }
+  await Order.save({ validateBeforeSave: false });
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+// update stock func
+const updateStock = async (productId, qty) => {
+  const product = await Product.findById(productId);
+  product.Stock = product.Stock - qty;
+  await Product.save({
+    validateBeforeSave: false,
+  });
+};
+
+// delete order
+exports.deleteOrder = catchErrors(async (req, res, next) => {
+  const deletedOrder = await Order.findById(req.params.id);
+  const result = await deletedOrder.deleteOne();
+  res.status(201).json({
+    success: true,
+    result,
+    message: "Order deleted succesfully.",
   });
 });
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -4,11 +4,29 @@ const orderController = require("../controllers/orderController");
 const { authenticatedUser, authorizedRoles } = require("../middleware/auth");
 
 router.post("/order/new", authenticatedUser, orderController.createOrder);
+router
+  .route("/admin/order/:id")
+  .get(
+    // "/order/:id",
+    authenticatedUser,
+    authorizedRoles("admin"),
+    orderController.getSingleOrder
+  )
+  .put(
+    authenticatedUser,
+    authorizedRoles("admin"),
+    orderController.updateOrderStatus
+  )
+  .delete(
+    authenticatedUser,
+    authorizedRoles("admin"),
+    orderController.deleteOrder
+  );
 router.get(
-  "/order/:id",
+  "/admin/order/total",
   authenticatedUser,
   authorizedRoles("admin"),
-  orderController.getSingleOrder
+  orderController.getTotalOrdersByAdmin
 );
 router.get(
   "/orders/myOrders",
